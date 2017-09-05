2b4db2af46488ba52651bc57463f408280b7a709
Steve Tarantini <steventarantini@gmail.com>
2017-09-05 12:02:10 -0400

    login fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/posts'
     else
-      redirect_to 'login'
+      redirect_to '/login'
     end
   end
   def destroy
