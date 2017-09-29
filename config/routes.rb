44dbf761ecfc87f7f32ee7d1e2097d22cbfebe10
Steve Tarantini <steventarantini@gmail.com>
2017-09-28 21:03:23 -0400

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
       resources :subscribers
   root 'welcome#home'
   root to: 'posts#index'
@@ -26,7 +27,6 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
   get "welcome/download_png"
   get "welcome/download_mp3"
-
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
