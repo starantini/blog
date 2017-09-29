f80537ede57874d123c68169c3092ce8ca6de4e1
Steve Tarantini <steventarantini@gmail.com>
2017-09-28 20:42:10 -0400

    fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   before_action :require_user, only: [:index, :show]
-  before_action :is_admin 
+  # before_action :is_admin 
   def index
     @posts = Post.order('created_at DESC')
   end
